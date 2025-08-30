7124450dc51025bcdae5925b46166669cbd3c1ca
Poekes <sunday26261@gmail.com>
2025-08-31 00:54:41 +0700

    feat(search): add search result page
    This commit adds a new search result page to display product search. It includes refactored logic for handle search queries

--- src/features/search/SearchPage.svelte
+++ src/features/search/SearchPage.svelte
@@ -1,22 +1,47 @@
 <script>
-    import { page } from "$app/stores";
-
-    // import { page } from "$app/state";
+    import { page as pageStores } from "$app/stores";
+    import { page } from "$app/state";
     import Navigasi from "$lib/components/Navigasi.svelte";
     import BouquetModel from "$lib/models/BouquetModel";
     import { onMount } from "svelte";
-    let produk = $state([]);
-    page.subscribe(({ params }) => {
-        const bouquet = new BouquetModel();
+    import CardRounded from "$lib/components/CardRounded.svelte";
+    import formatRupiah from "$lib/helper/formatRupiah";
+    import H1 from "$lib/components/H1.svelte";
+    import P from "$lib/components/P.svelte";
+    let produks = $state([]);
+    const bouquet = new BouquetModel();
 
-        console.log(params.search);
-        produk = bouquet.getSearch(params.search);
+    pageStores.subscribe((pg) => {
+        if (pg.route.id == "/search/[search]") {
+            const params = pg.params;
+            produks = bouquet.getSearch(params.search);
+        }
     });
 </script>
 
-<Navigasi />
-<main class="relative z-20">
-    {#each produk as item}
-        <h1>this search is {item.name}</h1>
-    {/each}
+<main class="font-sans m-auto w-full md:max-w-7xl z-20">
+    <Navigasi />
+    <section
+        class="py-12 pt-4 px-4 grid md:grid-cols-4 md:gap-2 grid-cols-1 z-20 relative"
+    >
+        <!-- Teks Section -->
+        <div class="col-span-1 md:max-w-96">
+            <H1>
+                Bouquet Murah Lucu dengan harga terjangkau mulai dari 20 ribu
+            </H1>
+
+            <P>
+                Mencari "{page.params.search}"
+            </P>
+        </div>
+
+        <div
+            class=" col-span-3 grid lg:grid-cols-2 grid-cols-2 gap-4 w-full sm:px-10 items-baseline md:py-0 py-4"
+        >
+            <!-- produk section -->
+            {#each produks as produk}
+                <CardRounded {...produk} price={formatRupiah(produk.price)} />
+            {/each}
+        </div>
+    </section>
 </main>
